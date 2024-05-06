719ccf4847860bcbe906754dd0530c1dde92883a
Dinh Duc <dinhducbkdn2004@gmail.com>
2024-05-06 10:27:14 +0700

    m

--- PBL3.Server/Repositories/EmployeeRepo.cs
+++ PBL3.Server/Repositories/EmployeeRepo.cs
@@ -102,87 +102,9 @@ namespace PBL3.Server.Repositories
 
         public async Task<ActionResult> GetEmployeeByIdAsync(int id)
         {
-<<<<<<< HEAD
-            try
-            {
-                var result = from employee in _context.Employees
-                             join duty in _context.Duties on employee.DutyId equals duty.Id
-                             select new
-                             {
-                                 Id = employee.Id,
-                                 FullName = employee.FullName,
-                                 Email = employee.Email,
-                                 PhoneNumber = employee.PhoneNumber,
-                                 TypeOfEmployee = employee.TypeOfEmployee,
-                                 CoefficientsSalary = employee.CoefficientsSalary,
-                                 Status = employee.Status,
-                                 DutyName = duty.DutyName
-                             };
-
-                return await result.ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Failed to retrieve all employees: {ex.Message}", ex);
-            }
-        }
-
-
-        public async Task<object> GetAllEmployeesByStatusAsync(bool status)
-        {
-            try
-            {
-                var result = from employee in _context.Employees
-                             join duty in _context.Duties on employee.DutyId equals duty.Id
-                             where employee.Status == status
-                             select new
-                             {
-                                 Id = employee.Id,
-                                 FullName = employee.FullName,
-                                 Email = employee.Email,
-                                 PhoneNumber = employee.PhoneNumber,
-                                 TypeOfEmployee = employee.TypeOfEmployee,
-                                 CoefficientsSalary = employee.CoefficientsSalary,
-                                 //DutyId = employee.DutyId,
-                                 Status = employee.Status,
-                                 DutyName = duty.DutyName
-                             };
-
-                return await result.ToListAsync();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Failed to retrieve all employees by status: {ex.Message}", ex);
-            }
-        }
-
-        public async Task<object> GetEmployeeByIdAsync(int id)
-        {
-            try
-            {
-                var result = from employee in _context.Employees
-                             join duty in _context.Duties on employee.DutyId equals duty.Id
-                             where employee.Id == id
-                             select new
-                             {
-                                 Id = employee.Id,
-                                 FullName = employee.FullName,
-                                 Email = employee.Email,
-                                 PhoneNumber = employee.PhoneNumber,
-                                 TypeOfEmployee = employee.TypeOfEmployee,
-                                 CoefficientsSalary = employee.CoefficientsSalary,
-                                 Status = employee.Status,
-                                 DutyName = duty.DutyName,
-                                 BasicSalary = duty.BasicSalary,
-                             };
-
-                var employeeInfo = await result.FirstOrDefaultAsync();
-                if (employeeInfo == null)
-=======
             var result = await _context.Employees
                 .Where(employee => employee.Id == id)
                 .Join(_context.Duties, employee => employee.DutyId, duty => duty.Id, (employee, duty) => new
->>>>>>> 6.5
                 {
                     Id = employee.Id,
                     FullName = employee.FullName,
