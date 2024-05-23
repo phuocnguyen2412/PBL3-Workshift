1b1c12dc10edeb1a69ecb74a5b956e212a6d69ce
phuocnguyen2412 <huynhphuocnguyen2412@gmail.com>
2024-05-23 18:17:07 +0700

    dark mode

--- pbl3.client/src/App.jsx
+++ pbl3.client/src/App.jsx
@@ -3,11 +3,23 @@ import { useRoutes } from "react-router-dom";
 import { AccountProvider } from "./Context/AccountContext";
 
 import routes from "./routes";
+import { ConfigProvider, theme } from "antd";
+import { useState } from "react";
 
 function App() {
     const routeList = useRoutes(routes);
-
-    return <AccountProvider>{routeList}</AccountProvider>;
+    const [darkMode, setDarkMode] = useState(true);
+    return (
+        <ConfigProvider
+            theme={{
+                algorithm: darkMode
+                    ? theme.darkAlgorithm
+                    : theme.compactAlgorithm,
+            }}
+        >
+            <AccountProvider>{routeList}</AccountProvider>
+        </ConfigProvider>
+    );
 }
 
 export default App;
